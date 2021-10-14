6b19417a9638893eebd4d20783098d9ebfe6a6ad
Danster4 <86613317+Danster4@users.noreply.github.com>
2021-10-14 13:57:58 -0400

    add ability to feature a project and refactor code

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js')
+const fs = require('fs');
+const generatePage = require('./src/page-template.js')
 
-// const pageHTML = generatePage(name, github);
-
-
-// fs.writeFile('index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Checkout out index.html to see the output!')
-// });
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -142,24 +134,15 @@ Add a New Project
   });
 };
 
+
 promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-    console.log(portfolioData);
+.then(promptProject)
+.then(portfolioData => {
+  const pageHTML = generatePage(portfolioData);
+
+  fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error(err);
+
+    console.log('Page created! Check out index.html in this directory to see it!');
   });
-
-
-
-
-
-
-
-
-
-
-
-// const printProfileData = (profileDataArr) => {
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs)
+});
